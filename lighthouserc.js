90b491bffabd0adfdb894d53474b427a510aca2c
MillsyCDXX <97089547+millsyuedev@users.noreply.github.com>
2025-08-27 11:12:11 +0100

    push

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npx http-server public -p 8000',
+      url: ['http://localhost:8000'],
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:accessibility': ['warn', { minScore: 0.8 }],
+        'categories:best-practices': ['warn', { minScore: 0.8 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
